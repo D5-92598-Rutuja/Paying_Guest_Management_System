83d18eedbd99b69ca989165d18eace484d30b27f
unknown <samiksha>
2025-11-15 22:01:39 +0530

    Initial setup done

---
+++ client/src/pages/Bookings/AllocateRoom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllocateRoom = () => {
+  return (
+    <div>
+      <h1>AllocateRoom</h1>
+    </div>
+  )
+}
+
+export default AllocateRoom
---
+++ client/src/pages/Payments/ViewPayments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ViewPayments = () => {
+  return (
+    <div>
+      <h1>View Payments</h1>
+    </div>
+  )
+}
+
+export default ViewPayments
