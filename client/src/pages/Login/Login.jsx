30ef44a27fafcac2ff84dd776d63a77bc7524269
Rutuja <rutuja4525@gmail.com>
2025-11-30 15:45:42 +0530

    Login page Added

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
-const Login = () => {
+function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-wrapper">
+      <div className="login-card">
+
+        <h2 className="title">Welcome Back</h2>
+        <p className="subtitle">Login to continue</p>
+
+        <form>
+          <div className="input-group">
+            <label>Mobile Number</label>
+            <input type="text" placeholder="Enter mobile number" />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" placeholder="Enter password" />
+          </div>
+
+          <button className="btn-login">Login</button>
+        </form>
+
+        <p className="register-text">
+          Not a user?
+          <span className="register-link"> Register</span>
+        </p>
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
