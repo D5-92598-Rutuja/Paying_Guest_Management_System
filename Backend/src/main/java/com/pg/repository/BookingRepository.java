f5fbca4dd3037e6929df4b5aeaaedface555c797
Vaishnavi Kalhapure <vaishnavi02kalhapure@gmail.com>
2026-02-03 14:16:50 +0530

    added logic for scheduling activateBookings() and findByStatusAndEndDateAfterOrNull()

--- Backend/src/main/java/com/pg/repository/BookingRepository.java
+++ Backend/src/main/java/com/pg/repository/BookingRepository.java
@@ -11,5 +11,29 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByStatus(BookingStatus status);
     
     // // PAYMENT - For recurring bills - Active bookings not expired
-    List<Booking> findByStatusAndEndDateAfter(BookingStatus status, LocalDate date);
-}
+     @Query("""
+    	    SELECT b FROM Booking b 
+    	    WHERE b.status = :status 
+    	      AND (
+    	        (b.endDate IS NULL) OR 
+    	        (b.endDate IS NOT NULL AND b.endDate > :today)
+    	      )
+    	    """)
+    	List<Booking> findByStatusAndEndDateAfterOrNull(
+    	    @Param("status") BookingStatus status, 
+    	    @Param("today") LocalDate today
+    	);
+
+//    List<Booking> findByStatusAndEndDateAfter(BookingStatus status, LocalDate date);
+    
+    @Modifying
+    @Transactional
+    @Query("""
+        UPDATE Booking b 
+        SET b.status = 'ACTIVE'
+        WHERE b.status = 'APPROVED'
+        AND b.status != 'ACTIVE'
+          AND CURRENT_DATE >= b.joinDate
+          AND (b.endDate IS NULL OR CURRENT_DATE <= b.endDate)
+        """)
+    int activateBookings();
