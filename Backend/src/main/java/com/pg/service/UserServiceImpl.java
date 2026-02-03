114d88842ee3a7eaf93caa9e5d95cd9d35da6f90
Rutuja <rutuja4525@gmail.com>
2026-02-03 16:03:25 +0530

    UserService modified

--- Backend/src/main/java/com/pg/service/UserServiceImpl.java
+++ Backend/src/main/java/com/pg/service/UserServiceImpl.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.pg.dtos.RegisterRequestDTO;
 import com.pg.dtos.UserRespDTO;
 import com.pg.entities.User;
+import com.pg.exception.EmailAlreadyExistsException;
 import com.pg.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,23 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+
+        //  REGISTER USER
+    @Override
+    public UserRespDTO registerUser(RegisterRequestDTO dto) {
+
+        if (userRepository.findByEmail(dto.getEmail()).isPresent()) { 
+        	throw new EmailAlreadyExistsException("Email already registered");
+        }
+
+        User user = modelMapper.map(dto, User.class);
+
+        user.setPassword(encoder.encode(dto.getPassword()));
+        user.setRole(Role.ROLE_USER);     
+        user.setStatus(Status.ACTIVE);
+
+        return modelMapper.map(userRepository.save(user), UserRespDTO.class);
+    }
 
     @Override
     public UserRespDTO createUser(User user) {
